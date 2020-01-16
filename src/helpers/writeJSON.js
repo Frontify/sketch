c74c6a21525299fdd901bffc88f6fb8e445423c8
Jamie <jamio.mitchell@gmail.com>
2020-01-16 16:22:56 +0100

    Applies prettier

--- src/helpers/writeJSON.js
+++ src/helpers/writeJSON.js
@@ -3,15 +3,14 @@ let fileManager = NSFileManager.defaultManager();
 export default function (key, data, path) {
     let jsContent = JSON.stringify(data);
     let jsContentNSSString = NSString.stringWithFormat("%@", jsContent);
-    let jsFolderPath =  path || '' + NSHomeDirectory() + '/Frontify/.config/';
+    let jsFolderPath = path || '' + NSHomeDirectory() + '/Frontify/.config/';
     let jsContentFilePath = jsFolderPath + key + '.json';
 
-    if(!fileManager.fileExistsAtPath(jsFolderPath)) {
-        if(fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error(jsFolderPath, true, null, null)) {
+    if (!fileManager.fileExistsAtPath(jsFolderPath)) {
+        if (fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error(jsFolderPath, true, null, null)) {
             jsContentNSSString.dataUsingEncoding_(NSUTF8StringEncoding).writeToFile_atomically_(jsContentFilePath, true);
         }
-    }
-    else {
+    } else {
         jsContentNSSString.dataUsingEncoding_(NSUTF8StringEncoding).writeToFile_atomically_(jsContentFilePath, true);
     }
 }
