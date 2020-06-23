cdb776e0356dc47a7512c53d572f82a2dd8d8f85
jonasbuechel <jonas@frontify.com>
2020-06-23 16:30:36 +0200

    Prettify
    Signed-off-by: jonasbuechel <jonas@frontify.com>

--- src/assets/patterns/organisms/login/js/login.js
+++ src/assets/patterns/organisms/login/js/login.js
@@ -1,58 +1,66 @@
 Tc.Module.Login = Tc.Module.extend({
-    on: function(callback) {
+    on: function (callback) {
         let $ctx = this.$ctx;
         this.settings = null;
 
         this.render();
 
-        $ctx.on('click', '.js-o-login__link', function(e) {
-            e.preventDefault();
-            let $this = $(e.currentTarget);
-            let url = $this.attr('href');
+        $ctx.on(
+            'click',
+            '.js-o-login__link',
+            function (e) {
+                e.preventDefault();
+                let $this = $(e.currentTarget);
+                let url = $this.attr('href');
 
-            window.postMessage('openUrl', url, true);
-        }.bind(this));
+                window.postMessage('openUrl', url, true);
+            }.bind(this)
+        );
 
         callback();
     },
 
-    render: function() {
+    render: function () {
         let $ctx = this.$ctx;
         $ctx.html(window.tpl.login());
 
         this.settings = new Tc.Module.Settings($ctx.find('.js-o-login__form'), this.sandbox);
-        this.settings.validation(function () {
-            let domain = this.sanitizeUrl(this.settings.serialize().domain);
-            let blacklist = ['https://app.frontify.com'];
+        this.settings.validation(
+            function () {
+                let domain = this.sanitizeUrl(this.settings.serialize().domain);
+                let blacklist = ['https://app.frontify.com'];
 
-            let permitted = true;
-            blacklist.forEach(function(item) {
-                if(item.startsWith(domain)) {
-                    permitted = false;
+                let permitted = true;
+                blacklist.forEach(
+                    function (item) {
+                        if (item.startsWith(domain)) {
+                            permitted = false;
+                        }
+                    }.bind(this)
+                );
+
+                if (!permitted) {
+                    this.settings.showErrors({ domain: 'Please use your dedicated Frontify domain' });
+                } else {
+                    this.login(domain);
                 }
-            }.bind(this));
-
-            if(!permitted) {
-                this.settings.showErrors({ domain: 'Please use your dedicated Frontify domain'})
-            }
-            else {
-                this.login(domain);
-            }
-        }.bind(this), function () {
-            // noop
-        }.bind(this));
+            }.bind(this),
+            function () {
+                // noop
+            }.bind(this)
+        );
     },
 
-    login: function(domain) {
+    login: function (domain) {
         window.postMessage('memorizeDomain', domain);
         window.postMessage('beginOauthFlow', domain);
     },
 
-    sanitizeUrl: function(url) {
+    sanitizeUrl: function (url) {
         // protocol
         let pattern = /^(ht)tps?:\/\//i;
         if (!pattern.test(url)) {
-            url = "https://" + url;
+            url = 'https://' + url;
         }
 
         // strip path
@@ -60,6 +68,5 @@ Tc.Module.Login = Tc.Module.extend({
         url = parts.slice(0, 3).join('/');
 
         return url;
-    }
-
+    },
 });
