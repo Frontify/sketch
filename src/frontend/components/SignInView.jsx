89cfd412817c00d81f8bedf7cad0a71ce5fa21a7
Florian Schulz <mail@getflourish.com>
2022-05-16 09:00:41 +0200

    Verify domains before starting the sign in process

--- src/frontend/components/SignInView.jsx
+++ src/frontend/components/SignInView.jsx
@@ -6,18 +6,67 @@ import { Logo } from './Logo';
 import { UserContext } from '../UserContext';
 import { useNavigate } from 'react-router-dom';
 
+function urlStartsWithProtocol(url) {
+    return url.indexOf('http') == 0;
+}
+
+function urlIsEmpty(url) {
+    return url.length == 0;
+}
+
+function urlIsBlackListed(url) {
+    if (url) {
+        var blacklist = ['http://app.frontify.com', 'https://app.frontify.com'];
+
+        var forbidden = false;
+        blacklist.forEach((entry) => {
+            if (url.startsWith(entry)) forbidden = true;
+        });
+        return forbidden;
+    }
+}
+
+function sanitizeURL(url) {
+    // protocol
+
+    let pattern = /^(ht)tps?:\/\//i;
+
+    if (!pattern.test(url)) {
+        url = 'https://' + url;
+    }
+    if (url.includes('http://')) {
+        url = url.replace('http://', 'https://');
+    }
+    // strip path
+    let parts = url.split('/');
+    url = parts.slice(0, 3).join('/');
+    return url;
+}
+
 export function SignInView() {
+    let placeholders = { domain: 'https://your.frontify.domain.tld' };
     const context = useContext(UserContext);
-    let [domain, setDomain] = useState('https://company-136571.frontify.com');
+    let [domain, setDomain] = useState('');
     const { t, i18n } = useTranslation();
     const navigate = useNavigate();
+    let [validDomain, setValidDomain] = useState(false);
+    let [validationError, setValidationError] = useState('');
 
     function handleSignIn(domain) {
-        navigate('/signin-pending');
-        let isValidDomain = true;
-        if (!isValidDomain) return;
-        window.postMessage('beginOauthFlow', domain);
+        if (validDomain) {
+            domain = sanitizeURL(domain);
+            navigate('/signin-pending');
+            window.postMessage('beginOauthFlow', domain);
+        }
     }
+
+    useEffect(() => {
+        if (!urlIsEmpty(domain) && !urlIsBlackListed(domain) && urlStartsWithProtocol(domain)) {
+            setValidDomain(true);
+        } else {
+            setValidDomain(false);
+        }
+    }, [domain]);
 
     return (
         <custom-v-stack gap="small" padding="small">
@@ -27,14 +76,21 @@ export function SignInView() {
             <Text>{t('signin.your_frontify_domain')}</Text>
             <fieldset>
                 <TextInput
+                    onEnterPressed={(event) => {
+                        console.log(event.target.value);
+                        setDomain(event.target.value);
+                        handleSignIn(event.target.value);
+                    }}
                     type="text"
-                    placeholder="https://domain"
+                    placeholder={placeholders.domain}
                     value={domain}
                     onChange={(value) => setDomain(value)}
                 />
             </fieldset>
             <div>
-                <Button onClick={(event) => handleSignIn(domain)}>{t('signin.sign_in')}</Button>
+                <Button disabled={!validDomain} onClick={(event) => handleSignIn(domain)}>
+                    {t('signin.sign_in')}
+                </Button>
             </div>
             <custom-spacer></custom-spacer>
             <Text>{t('signin.need_help')}</Text>
