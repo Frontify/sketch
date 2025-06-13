d3e93732005be7ab1e5e1e9293de018f530f6305
Margus Lanto <margus.lanto@frontify.com>
2025-06-13 10:08:49 +0200

    Comment out likely outdated logic of notarization script

--- notarize.sh
+++ notarize.sh
@@ -13,17 +13,19 @@ echo "Notarizing "$ZIP" --keychain-profile "Sketch""
 
 OUTPUT=$(xcrun notarytool submit $ZIP --keychain-profile "Sketch" --wait)
 
-FIRST_LINE=$(echo "$OUTPUT" | sed -n '1p')
+echo "$OUTPUT"
 
-if [[ $FIRST_LINE != "No errors uploading"* ]];
-then
-    echo "Problems with the notarization process. Check the log bellow"
-    echo "$OUTPUT"
-    exit 1
-else
-    SECOND_LINE=$(echo "$OUTPUT" | sed -n '2p')
-    UUID=$(echo $SECOND_LINE| cut -d '=' -f 2)
+# FIRST_LINE=$(echo "$OUTPUT" | sed -n '1p')
 
-    echo "Plugin sent to the notarization server. To check the status of the request (might take a few minutes), run:"
-    echo "xcrun notarytool log "$UUID" --keychain-profile "Sketch" developer_log.json"
-fi
+# if [[ $FIRST_LINE != "No errors uploading"* ]];
+# then
+#     echo "Problems with the notarization process. Check the log bellow"
+#     echo "$OUTPUT"
+#     exit 1
+# else
+#     SECOND_LINE=$(echo "$OUTPUT" | sed -n '2p')
+#     UUID=$(echo $SECOND_LINE| cut -d '=' -f 2)
+
+#     echo "Plugin sent to the notarization server. To check the status of the request (might take a few minutes), run:"
+#     echo "xcrun notarytool log "$UUID" --keychain-profile "Sketch" developer_log.json"
+# fi
